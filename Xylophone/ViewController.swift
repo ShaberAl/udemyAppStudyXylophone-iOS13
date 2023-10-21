8b865c935a16b6724c19ac69d2ca29eba3bd3863
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-10-21 12:46:29 +0500

    init

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -9,13 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+    }
 }
 
