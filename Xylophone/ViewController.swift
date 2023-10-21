98e0524bb4c477c615f1e11801b499696eb2a3c1
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-10-21 14:19:57 +0500

    Ready

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: UIViewController {
         playSound(for: sender.currentTitle)
     }
     
-    func playSound(for buttonLabelText: String?) {
-        guard let url = Bundle.main.url(forResource: buttonLabelText, withExtension: "wav") else { return }
+    func playSound(for soundName: String?) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
         
         player = try! AVAudioPlayer(contentsOf: url)
         player?.play()
